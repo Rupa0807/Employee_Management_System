d6b86d9b9a4e71e7c309e4c4c44bf3f6e9b2e653
Rupa0807 <rupa0807@gmail.com>
2024-09-03 23:30:34 +0100

    add controller

--- Employee_Management_System/src/main/java/com/ems/Controller/EmployeeController.java
+++ Employee_Management_System/src/main/java/com/ems/Controller/EmployeeController.java
@@ -2,9 +2,10 @@ package com.ems.Controller;
 
 import com.ems.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class EmployeeController {
 @Autowired
     EmployeeService employeeService;
